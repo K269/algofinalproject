d98dcf336de8808b4287d568af1a66e89230d21b
LihiZXC <106438223+LihiZXC@users.noreply.github.com>
2022-06-18 04:33:02 +0300

    че
    предлагаю залить прон в наш проект

---
+++ Я доделал это в 4 утра кста, пс Владимир был здесь.py
@@ -0,0 +1,40 @@
+img_hero = ""
+img_enemy = ""
+img_bullet = ""
+#сверху ковычках должны находится загруденные в программу спрайты и их название.png
+class GameSprite(sprite.Sprite):
+   def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+       sprite.Sprite.__init__(self)
+       self.image = transform.scale(image.load(player_image), (size_x, size_y))
+       self.speed = player_speed
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+   def update(self):
+       keys = key.get_pressed()
+       if keys[K_LEFT] and self.rect.x > 5:
+           self.rect.x -= self.speed
+       if keys[K_RIGHT] and self.rect.x < win_width - 80:
+           self.rect.x += self.speed
+   def fire(self):
+       bullet = Bullet(img_bullet,self.rect.centerx, self.rect.top,75,60,-10)
+       bullets.add(bullet)
+# вышел мужик на балкон, а балкона и нет.
+class Enemy(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        global lost
+        if self.rect.y > win_height:
+            self.rect.x = randint(80,win_width - 80)
+            self.rect.y = 0
+            lost = lost + 1
+
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.y < 0:
+            self.kill()
